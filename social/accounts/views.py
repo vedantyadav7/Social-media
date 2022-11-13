48eeea1e1e22bb2b8f65a6183fb1f671ba109320
vedantyadav7 <vedant.yadav7@gmail.com>
2022-11-13 22:14:34 +0530

    social media push code

---
+++ social/accounts/views.py
@@ -0,0 +1,12 @@
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from . import forms
+from django.contrib.auth import login, logout
+# Create your views here.
+
+class SignUp(CreateView):
+    form_class = forms.UserCreateForm
+    success_url = reverse_lazy("login")
+    template_name = "account/signup.html" 
+
+
